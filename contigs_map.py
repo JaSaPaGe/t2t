1249adc5554d821ac39bc23f677cbb8b4022a4c5
Maxat Kulmanov <coolmaksat@gmail.com>
2023-09-07 14:59:20 +0300

    divided the scripts

---
+++ contigs_map.py
@@ -0,0 +1,125 @@
+import click as ck
+import math
+import os
+import numpy as np
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+import json
+
+class Align(object):
+
+    def __init__(self, chr_id, chr_len, chr_start, chr_end,
+                 ctg_id, ctg_len, ctg_start, ctg_end, is_reverse=False):
+        self.chr_id = chr_id
+        self.chr_len = chr_len
+        self.chr_start = chr_start
+        self.chr_end = chr_end
+        self.ctg_id = ctg_id
+        self.ctg_len = ctg_len
+        self.ctg_start = ctg_start
+        self.ctg_end = ctg_end
+        self.is_reverse = is_reverse
+
+    def toarray(self):
+        if self.is_reverse:
+            return [f'-{self.ctg_id}', self.ctg_start, self.ctg_end]
+        else:
+            return [self.ctg_id, self.ctg_start, self.ctg_end]
+        
+@ck.command()
+@ck.option('--alignment-file', '-af', default='verkko_trio_alignment_hap1.txt', help='Filtered alignments from filter_alignment.py')
+@ck.option('--output', '-o', default='', help='Output JSON file with configs')
+def main(alignment_file, output):
+    aligns = {}
+    with open(alignment_file) as f:
+        for line in f:
+            it = line.strip().split()
+            ch = it[0]
+            ch_len = int(it[1])
+            ch_start, ch_end = int(it[2]), int(it[3])
+            ctg = it[8]
+            ctg_len = int(it[4])
+            ctg_start, ctg_end = int(it[5]), int(it[6])
+            reverse = it[-1] == '-'
+            if reverse:
+                ctg_start = ctg_len - ctg_end
+                ctg_end = ctg_len - int(it[5])
+            if ch not in aligns:
+                aligns[ch] = []
+            aligns[ch].append(Align(
+                ch, ch_len, ch_start, ch_end, ctg,
+                ctg_len, ctg_start, ctg_end, reverse))
+
+    chroms = [f'chr{i}' for i in range(1, 23)]
+    chroms.append('chrX')
+    chroms.append('chrY')
+    chroms.append('chrM')
+
+    res = {}
+    for ch in chroms:
+        start_id = ch + '_s'
+        end_id = ch + '_e'
+        if start_id in aligns:
+            ctgs = get_contigs(ch, aligns[start_id], aligns[end_id])
+        elif end_id in aligns:
+            ctgs = get_contigs(ch, None, aligns[end_id])
+        res.update(ctgs)
+
+    with open(output, 'w') as f:
+        f.write(json.dumps(res, indent=4))
+    
+    
+def get_contigs(chrom, start, end):
+    cen_contigs = []
+    result = {}
+    if start is not None:
+        start_contigs = join_contigs(start)
+        if start_contigs[0].ctg_start < 20000:
+            start_contigs[0].ctg_start = 0
+        result[chrom + '_s'] =  [ctg.toarray() for ctg in start_contigs]
+        ctg = start_contigs[-1]
+        if ctg.ctg_end < ctg.ctg_len:
+            align = Align(
+                chrom + '_c', -1, -1, -1, ctg.ctg_id, ctg.ctg_len, ctg.ctg_end, ctg.ctg_len, ctg.is_reverse)
+            cen_contigs.append(align)
+    end_contigs = join_contigs(end)
+    ctg = end_contigs[0]
+    if ctg.ctg_start > 0:
+        if len(cen_contigs) > 0 and cen_contigs[0].ctg_id == ctg.ctg_id:
+            cen_contigs[0].ctg_end = ctg.ctg_start
+        else:
+            align = Align(
+                chrom + '_c', -1, -1, -1, ctg.ctg_id, ctg.ctg_len, 0, ctg.ctg_start, ctg.is_reverse)
+            cen_contigs.append(align)
+
+    result[chrom + '_c'] = [ctg.toarray() for ctg in cen_contigs]
+    result[chrom + '_e'] = [ctg.toarray() for ctg in end_contigs]
+    return result
+
+
+def join_contigs(contigs):
+    joined = []
+    joined.append(contigs[0])
+    ind = 1
+    while ind < len(contigs):
+        if joined[-1].ctg_id == contigs[ind].ctg_id:
+            if joined[-1].chr_end < contigs[ind].chr_end:
+                joined[-1].ctg_end = contigs[ind].ctg_end
+                joined[-1].chr_end = contigs[ind].chr_end
+        elif joined[-1].chr_end < contigs[ind].chr_end:
+            if joined[-1].chr_start < contigs[ind].chr_start:
+                if joined[-1].chr_end > contigs[ind].chr_start:
+                    diff = joined[-1].chr_end - contigs[ind].chr_start
+                    contigs[ind].chr_start = joined[-1].chr_end
+                    contigs[ind].ctg_start += diff
+                joined.append(contigs[ind])
+            else:
+                joined[-1] = contigs[ind]
+        ind += 1
+    return joined
+
+
+
+if __name__ == '__main__':
+    main()
